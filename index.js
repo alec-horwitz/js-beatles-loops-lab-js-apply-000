329d41a39a0cc0303599877fddb0d673641b5eed
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-01-03 22:19:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function theBeatlesPlay(musicians, instruments) {
+  var newArray = [];
+  for (var i = 0; i < musicians.length; i++) {
+    if (i < instruments.length) {
+      newArray.push(musicians[i] + " plays " + instruments[i]);
+    } else {
+      newArray.push(musicians[i] + " wasn't assigned an instrument");
+    }
+  }
+  return newArray;
+}
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  var newArray = [];
+  while (i < facts.length) {
+    newArray.push(facts[i] + "!!!");
+  }
+  return newArray;
+}
